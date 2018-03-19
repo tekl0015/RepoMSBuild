777c92bd5d88de592e5e3db9b7e5c95b7771193b
tekl0015 <tekl0015@umn.edu>
2018-03-19 13:29:19 -0500

    SrcCgeee

--- Jenkin_Git_Test/Program.cs
+++ Jenkin_Git_Test/Program.cs
@@ -23,7 +23,7 @@ namespace Jenkin_Git_Test
                 LogFileWriter.WriteLine("Main: Jenkins - MSBuild - :" + i + Environment.NewLine);
             }
 
-            Console.WriteLine("...........................");
+            Console.WriteLine("##################################");
             LogFileWriter.Flush();
             LogFileWriter.Close();
         }
