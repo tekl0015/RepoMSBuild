eb6661df3b3a9888e1cec96ab81abb55bd643129
tekl0015 <tekl0015@umn.edu>
2018-03-19 13:15:51 -0500

    SrcC

--- Jenkin_Git_Test/Program.cs
+++ Jenkin_Git_Test/Program.cs
@@ -18,7 +18,7 @@ namespace Jenkin_Git_Test
             StreamWriter LogFileWriter = new StreamWriter(LogFileStream);
             
 
-            for (int i = 0; i < 10; i++)
+            for (int i = 0; i < 7; i++)
             {
                 LogFileWriter.WriteLine("Main: Jenkins - MSBuild - :" + i + Environment.NewLine);
             }
